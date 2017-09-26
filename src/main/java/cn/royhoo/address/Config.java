58a1e63f6b1ce6085eff389dc154af5c28ffafc2
royhoo <237042253@qq.com>
2017-09-26 23:58:01 +0800

    解析区划字典--未完成

---
+++ src/main/java/cn/royhoo/address/Config.java
@@ -0,0 +1,16 @@
+package cn.royhoo.address;
+
+/**
+ * @author royhoo
+ * @date 2017/9/26
+ */
+public class Config {
+    /**
+     * 区划地名词典路径
+     */
+    public static String DivisionPlaceDictionaryPath = "data/DivisionPlace.txt";
+    /**
+     * 区划地名关键字词典路径
+     */
+    public static String DivisionPlaceKeyWordPath = "data/DivisionPlaceKeyWord.txt";
+}
---
+++ src/main/java/cn/royhoo/address/dictionary/DivisionPlaceDictionary.java
@@ -0,0 +1,20 @@
+package cn.royhoo.address.dictionary;
+
+import java.io.Serializable;
+
+/**
+ * 区划地名词典
+ * @author royhoo
+ * @date 2017/9/26
+ */
+public class DivisionPlaceDictionary {
+    
+
+    /**
+     * 区划地名词属性
+     */
+    static public class Attribute implements Serializable{
+        // 待完善
+    }
+
+}
